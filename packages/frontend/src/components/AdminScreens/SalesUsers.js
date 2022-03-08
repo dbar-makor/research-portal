286bf69f3c42a6b5c26f195c0cf3dac8a2f4d960
dbar-makor <dbar-makor@outlook.com>
2022-03-08 22:12:31 +0200

    refactor: 💡 Debug config files

--- packages/frontend/src/components/AdminScreens/SalesUsers.js
+++ packages/frontend/src/components/AdminScreens/SalesUsers.js
@@ -1,134 +1,140 @@
-import { useState, useEffect, useRef, useCallback } from 'react'
-import { Grid, Typography } from '@material-ui/core'
-import { useStyles } from '../../styles/MainStyles'
-import SubHeader from '../Reusables/SubHeader'
-import TableComponent from '../Reusables/TableComponent'
+import { useState, useEffect, useRef, useCallback } from 'react';
+import { Grid, Typography } from '@material-ui/core';
+import { useStyles } from '../../styles/MainStyles';
+import SubHeader from '../Reusables/SubHeader';
+import TableComponent from '../Reusables/TableComponent';
 import {
-  selectSalesUsersData,
-  setUserProperty,
-  selectUsersOffset,
-  selectUsersLimit,
-  selectUsersLoading,
-  selectUsersMetaData,
-  selectUsersHasMore
-} from '../../redux/users/usersSlice'
-import { useDispatch, useSelector } from 'react-redux'
-import CircularProgress from '@material-ui/core/CircularProgress'
-import Filters from '../Reusables/Filters'
-import UserInfo from '../AdminScreens/UserInfo'
-import MembersTable from '../SalesScreens/MembersDetails/MembersTable'
+	selectSalesUsersData,
+	setUserProperty,
+	selectUsersOffset,
+	selectUsersLimit,
+	selectUsersLoading,
+	selectUsersMetaData,
+	selectUsersHasMore,
+} from '../../redux/users/usersSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Filters from '../Reusables/Filters';
+import UserInfo from './UserInfo';
+import MembersTable from '../SalesScreens/MembersDetails/MembersTable';
 import AwesomeDebouncePromise from 'awesome-debounce-promise';
-import { selectChosenUserData  } from '../../redux/users/chosenUserSlice'
+import { selectChosenUserData } from '../../redux/users/chosenUserSlice';
 
 function SalesUsers() {
-  const classes = useStyles()
-  const dispatch = useDispatch()
-  const salesData = useSelector(selectSalesUsersData)
-  const chosenUser = useSelector(selectChosenUserData)
-  
-  const loading = useSelector(selectUsersLoading)
-  const metaData = useSelector(selectUsersMetaData)
- 
-  const userOffset = useSelector(selectUsersOffset) 
-  const userLimit = useSelector(selectUsersLimit)  
-  // const scrollRef = useRef();
-  const hasMore = useSelector(selectUsersHasMore)
+	const classes = useStyles();
+	const dispatch = useDispatch();
+	const salesData = useSelector(selectSalesUsersData);
+	const chosenUser = useSelector(selectChosenUserData);
 
-  // const fetchCompaniesData = AwesomeDebouncePromise(dispatch(getCompaniesDataAsync(offset, limit, search, type, status)), 500)
+	const loading = useSelector(selectUsersLoading);
+	const metaData = useSelector(selectUsersMetaData);
 
-  // useEffect(() => {
-  //   dispatch(getUsersByTypeAsync('sales'))
-    
-  //   // fetchCompaniesData()
-  // }, [])
+	const userOffset = useSelector(selectUsersOffset);
+	const userLimit = useSelector(selectUsersLimit);
+	// const scrollRef = useRef();
+	const hasMore = useSelector(selectUsersHasMore);
 
-  // useEffect(() => {
-  //   dispatch(setProperty({key: 'offset', value: 0}))
-    
-  //   // fetchCompaniesData()
-  // }, [search, type, status])
+	// const fetchCompaniesData = AwesomeDebouncePromise(dispatch(getCompaniesDataAsync(offset, limit, search, type, status)), 500)
 
+	// useEffect(() => {
+	//   dispatch(getUsersByTypeAsync('sales'))
 
-  const observer = useRef(null);
+	//   // fetchCompaniesData()
+	// }, [])
 
-  const lastItemRef = useCallback(node => {
-    console.log("lastItemRef cb runs. node:", node);
+	// useEffect(() => {
+	//   dispatch(setProperty({key: 'offset', value: 0}))
 
-    if(loading){
-      return;
-    };
-    if(observer.current){
-      observer.current.disconnect();
-    }
+	//   // fetchCompaniesData()
+	// }, [search, type, status])
 
-    observer.current = new IntersectionObserver(entries => {
-    
-      if(entries[0].isIntersecting && hasMore){
+	const observer = useRef(null);
 
-        let newOffset = userOffset + userLimit;
-        // console.log("containerRef.current?.scrollHeight",containerRef.current?.scrollHeight)
-        dispatch(setUserProperty({key: 'offset', value: newOffset}))
-      }
-    });
-    if(node){
-      observer.current.observe(node);
-    }
+	const lastItemRef = useCallback(
+		(node) => {
+			console.log('lastItemRef cb runs. node:', node);
 
-  }, [loading, hasMore]) 
+			if (loading) {
+				return;
+			}
+			if (observer.current) {
+				observer.current.disconnect();
+			}
 
-  return (
-    <Grid container justifyContent='center'>
-      <Grid item xs={10}>
-        <Grid container>
-          
-          {!loading ? (
-              <Grid item xs={12}>
-                {salesData.length
-                ? <Grid container >
-                  <Grid
-                    item
-                    xs={6}
-                    className={classes.scrollableTableContainer}
-                    // onWheel={(e) => setWheel(e.nativeEvent.wheelDelta)}
-                    // onScroll={(e) => handleScroll(e)}
-                  >
-                    <TableComponent data={salesData} pageType='salesUsers' ref={lastItemRef}/>
-                  </Grid>
-                  {chosenUser && chosenUser.type === 'sales' &&
-                  <Grid item xs={6}>
-                    <Grid container style={{ padding: '0px 16px 0px 16px' }}>
-                      <Grid item xs={12}>
-                        <UserInfo />
-                      </Grid>
-                      {/* <Grid item xs={12}>
+			observer.current = new IntersectionObserver((entries) => {
+				if (entries[0].isIntersecting && hasMore) {
+					let newOffset = userOffset + userLimit;
+					// console.log("containerRef.current?.scrollHeight",containerRef.current?.scrollHeight)
+					dispatch(setUserProperty({ key: 'offset', value: newOffset }));
+				}
+			});
+			if (node) {
+				observer.current.observe(node);
+			}
+		},
+		[loading, hasMore],
+	);
+
+	return (
+		<Grid container justifyContent="center">
+			<Grid item xs={10}>
+				<Grid container>
+					{!loading ? (
+						<Grid item xs={12}>
+							{salesData.length ? (
+								<Grid container>
+									<Grid
+										item
+										xs={6}
+										className={classes.scrollableTableContainer}
+										// onWheel={(e) => setWheel(e.nativeEvent.wheelDelta)}
+										// onScroll={(e) => handleScroll(e)}
+									>
+										<TableComponent
+											data={salesData}
+											pageType="salesUsers"
+											ref={lastItemRef}
+										/>
+									</Grid>
+									{chosenUser && chosenUser.type === 'sales' && (
+										<Grid item xs={6}>
+											<Grid container style={{ padding: '0px 16px 0px 16px' }}>
+												<Grid item xs={12}>
+													<UserInfo />
+												</Grid>
+												{/* <Grid item xs={12}>
                         <MembersTable />
                       </Grid> */}
-                    </Grid>
-                  </Grid>}
-                </Grid>
-                : <Grid container>
-                  <Grid item xs={6}>
-                    <Typography className={classes.noMatches}>No matches found</Typography>
-                  </Grid>                    
-                  </Grid>}
-                
-              </Grid>
-          ) : (
-            <Grid item xs={12}>
-              <Grid
-                container
-                justifyContent='center'
-                alignItems='center'
-                className={classes.progressBarContainer}
-              >
-                <CircularProgress className={classes.progressBar} />
-              </Grid>
-            </Grid>
-          )}
-        </Grid>
-      </Grid>
-    </Grid>
-  )
+											</Grid>
+										</Grid>
+									)}
+								</Grid>
+							) : (
+								<Grid container>
+									<Grid item xs={6}>
+										<Typography className={classes.noMatches}>
+											No matches found
+										</Typography>
+									</Grid>
+								</Grid>
+							)}
+						</Grid>
+					) : (
+						<Grid item xs={12}>
+							<Grid
+								container
+								justifyContent="center"
+								alignItems="center"
+								className={classes.progressBarContainer}
+							>
+								<CircularProgress className={classes.progressBar} />
+							</Grid>
+						</Grid>
+					)}
+				</Grid>
+			</Grid>
+		</Grid>
+	);
 }
 
-export default SalesUsers
+export default SalesUsers;
--- packages/frontend/src/components/Reusables/CategoriesAutoComplete.js
+++ packages/frontend/src/components/Reusables/CategoriesAutoComplete.js
@@ -1,153 +1,144 @@
-import { useEffect } from 'react'
+import { useEffect } from 'react';
 import { Grid, Typography, TextField, Chip, makeStyles } from '@material-ui/core';
 import { Autocomplete } from '@material-ui/lab';
 import ClearIcon from '@material-ui/icons/Clear';
-import { useState} from 'react';
-import { useDispatch, useSelector} from "react-redux";
-import { StyledTextField, StyledAutoComplete } from '../../styles/MainStyles'
-import { validateMember } from "../Reusables/validationFunctions"
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { StyledTextField, StyledAutoComplete } from '../../styles/MainStyles';
+import { validateMember } from './validationFunctions';
 
+function CategoriesAutoComplete({
+	formObject,
+	setFormObject,
+	handler,
+	label = '',
+	className = '',
+	chipClassName,
+	chipContainerClassName,
+	error = null,
+	errors = null,
+	setErrors = () => {},
+	setValidationResult = () => {},
+	additionalParameter = null,
+	parentArr = null,
+	setParentArr = () => {},
+	chipVariant = 'outlined',
+}) {
+	const classes = useStyles();
+	const dispatch = useDispatch();
+	const categoriesArr = useSelector((state) => state.utils.utils.category);
 
-function CategoriesAutoComplete({formObject, 
-                                 setFormObject,
-                                 handler, 
-                                 label = '', 
-                                 className = '', 
-                                 chipClassName, 
-                                 chipContainerClassName,
-                                 error = null,
-                                 errors = null,
-                                 setErrors = () => {},
-                                 setValidationResult = () => {},
-                                 additionalParameter = null,
-                                 parentArr = null,
-                                 setParentArr = () => {},
-                                chipVariant='outlined' }){
+	useEffect(() => {
+		return () => {
+			console.log('autocom unmounted');
+		};
+	}, []);
 
-    const classes = useStyles();
-    const dispatch = useDispatch();
-    const categoriesArr = useSelector(state => state.utils.utils.category)
+	const categoriesIDSArr = categoriesArr?.map((item) => item.id);
+	const itemIndex =
+		parentArr && parentArr.length && parentArr.findIndex((item) => item.id === formObject.id);
 
- 
+	//always an arr:
+	const adjustedFormObject = formObject.categories ? formObject.categories : formObject;
 
-  useEffect(() => {
-    
-    return () => {
-    console.log("autocom unmounted")
-    }
-  }, [])
+	const deleteItem = (index) => {
+		const categoryCopy = [...adjustedFormObject];
+		categoryCopy.splice(index, 1);
+		const formObjectCopy = { ...formObject, categories: categoryCopy };
 
-    const categoriesIDSArr = categoriesArr?.map(item => item.id);
-    const itemIndex = parentArr && parentArr.length && parentArr.findIndex(item => item.id === formObject.id)
+		setFormObject(formObjectCopy);
+		if (parentArr?.length) {
+			const parentArrCopy = [...parentArr];
+			parentArrCopy.splice(itemIndex, 1, formObjectCopy);
+			setParentArr(parentArrCopy);
+		}
+		errors && validateMember({ categories: categoryCopy }, errors, setErrors, setValidationResult);
+	};
 
- //always an arr:
- const adjustedFormObject = formObject.categories ? formObject.categories : formObject;
-
-  const deleteItem = (index) => {
-    const categoryCopy  = [...adjustedFormObject];
-    categoryCopy.splice(index, 1);
-    const formObjectCopy = {...formObject, categories: categoryCopy}
-  
-    setFormObject(formObjectCopy);
-    if (parentArr?.length) {
-      const parentArrCopy = [...parentArr]
-      parentArrCopy.splice(itemIndex, 1, formObjectCopy)
-      setParentArr(parentArrCopy)
-    }
-    errors && validateMember({categories: categoryCopy}, errors, setErrors, setValidationResult)
-  };
- 
-  return (
-    <>
-    {categoriesArr && <Grid container className={className}>
-        {label && 
-          <Grid item xs={12}>
-            <Typography>{label}</Typography>
-          </Grid>
-        }
-        <Grid item xs={12}>
-          <StyledAutoComplete
-            className={classes.arrowIcon}
-            id="categories"
-            name="categories"
-            multiple
-            filterSelectedOptions
-            options={categoriesArr}
-            renderTags={() => <></>}
-            fullWidth
-            value={formObject.categories ? formObject.categories : formObject || ""}
-           
-            onChange={((e, values) => handler(values))}
-            {...(error && {error: true, helperText:error})}
-            // onInputChange={(event, newInputValue) => {
-            //   setInputValue(newInputValue);
-            // }}
-            // inputValue={inputValue}
-            getOptionLabel={(option) => option.name || ""}
-            getOptionSelected={(option, value) =>
-              option.name === value.name
-            }
-            renderInput={(params) => {
-              return (
-                <TextField
-                  {...params}
-                  autoComplete="off"
-                  variant="outlined"
-                  width="100%"
-                  placeholder="Categories"
-                  {...(error && {error: true, helperText:error})}
-                />
-              );
-            }}
-          />
-          {adjustedFormObject.length ? (
-          // {formObject.categories && formObject.categories.length ? (
-          // {formObject.members[memberIndex].categories && formObject.members[memberIndex].categories.length ? (
-            <Grid item xs={12}>
-              <Grid container className={classes.chipContainer}>
-                {adjustedFormObject.map((el, index) => (
-                  <Grid
-                    item
-                    key={index}
-                    className={classes.chipItem}
-                  >
-                    <Chip
-                      variant={chipVariant}
-                      label={el.name}
-                      onDelete={() => deleteItem(index)}
-                      deleteIcon={<ClearIcon />}
-                      className={classes.chip}
-                    />
-                  </Grid>
-                ))}
-              </Grid>
-              </Grid>
-            ) : null}
-          
-        </Grid>
-          </Grid>
- }
-    </>
-  );
+	return (
+		<>
+			{categoriesArr && (
+				<Grid container className={className}>
+					{label && (
+						<Grid item xs={12}>
+							<Typography>{label}</Typography>
+						</Grid>
+					)}
+					<Grid item xs={12}>
+						<StyledAutoComplete
+							className={classes.arrowIcon}
+							id="categories"
+							name="categories"
+							multiple
+							filterSelectedOptions
+							options={categoriesArr}
+							renderTags={() => <></>}
+							fullWidth
+							value={formObject.categories ? formObject.categories : formObject || ''}
+							onChange={(e, values) => handler(values)}
+							{...(error && { error: true, helperText: error })}
+							// onInputChange={(event, newInputValue) => {
+							//   setInputValue(newInputValue);
+							// }}
+							// inputValue={inputValue}
+							getOptionLabel={(option) => option.name || ''}
+							getOptionSelected={(option, value) => option.name === value.name}
+							renderInput={(params) => {
+								return (
+									<TextField
+										{...params}
+										autoComplete="off"
+										variant="outlined"
+										width="100%"
+										placeholder="Categories"
+										{...(error && { error: true, helperText: error })}
+									/>
+								);
+							}}
+						/>
+						{adjustedFormObject.length ? (
+							// {formObject.categories && formObject.categories.length ? (
+							// {formObject.members[memberIndex].categories && formObject.members[memberIndex].categories.length ? (
+							<Grid item xs={12}>
+								<Grid container className={classes.chipContainer}>
+									{adjustedFormObject.map((el, index) => (
+										<Grid item key={index} className={classes.chipItem}>
+											<Chip
+												variant={chipVariant}
+												label={el.name}
+												onDelete={() => deleteItem(index)}
+												deleteIcon={<ClearIcon />}
+												className={classes.chip}
+											/>
+										</Grid>
+									))}
+								</Grid>
+							</Grid>
+						) : null}
+					</Grid>
+				</Grid>
+			)}
+		</>
+	);
 }
 
 export default CategoriesAutoComplete;
 
 const useStyles = makeStyles((theme) => ({
-  chipContainer: {
-    marginTop: "10px",
-    },
-  chipItem: {
-  marginBottom: "5px",
-  marginRight: "3px",
-      },
-  chip: {
-  backgroundColor: "#EDEFF3",
-  border: "none"
-      },
-  arrowIcon: {
-    "& .MuiSvgIcon-root": {
-      color: "#1C67FF",
-    },
-  },
+	chipContainer: {
+		marginTop: '10px',
+	},
+	chipItem: {
+		marginBottom: '5px',
+		marginRight: '3px',
+	},
+	chip: {
+		backgroundColor: '#EDEFF3',
+		border: 'none',
+	},
+	arrowIcon: {
+		'& .MuiSvgIcon-root': {
+			color: '#1C67FF',
+		},
+	},
 }));

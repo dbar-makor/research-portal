38c045b739207a02b137b02ff3606c3e5a9c8f2b
Michal Tal-Socher <80818338+noriktal@users.noreply.github.com>
2022-03-20 16:42:24 +0200

    fix: 🐞 bug fixes in main admin screen (created by splitting comp

--- packages/backend/server_generate/sql/migration/2021-11-28/Dump20211128.sql
+++ packages/backend/server_generate/sql/migration/2021-11-28/Dump20211128.sql
@@ -251,7 +251,7 @@ DELIMITER ;
 DROP TABLE IF EXISTS `company`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `company` (
+CREATE TABLE `company` ( 
   `id` int unsigned NOT NULL AUTO_INCREMENT,
   `uuid` varchar(36) NOT NULL,
   `legal_name` text NOT NULL,

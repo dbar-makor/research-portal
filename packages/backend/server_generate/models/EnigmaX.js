1c76fec86f6751be6705e7a30f130eb1a1192340
ochayoun-makor <ochayoun@makor-capital.com>
2022-04-11 08:57:48 +0300

    test: ✅ update yarn

--- packages/backend/server_generate/models/EnigmaX.js
+++ packages/backend/server_generate/models/EnigmaX.js
@@ -7,13 +7,21 @@ EnigmaX.enigmaXGetPublications = async (payload, result) => {
 	try {
 		const publications = await db_helper.get(query.get_publications());
 		if (!publications) return result({ status: 404 });
-		console.log(publications);
 		if (publications.length) {
 			for (publication of publications) {
+				if (publication.type === 'live') {
+					delete publication.file_pdf;
+					delete publication.title_pdf;
+					delete publication.title_video;
+					delete publication.link_video;
+					publication.content = JSON.parse(publication.content);
+				}
+				if (publication.type === 'dead') {
+					delete publication.content;
+				}
 				delete publication.user_id;
 				const id = publication.real_id;
 				delete publication.real_id;
-				publication.content = JSON.parse(publication.content);
 				const tags = await db_helper.get(query.get_tags_by_publication_id(id));
 				publication.tags = tags;
 				const events = await db_helper.get(query.get_events_by_publication_id(id));
